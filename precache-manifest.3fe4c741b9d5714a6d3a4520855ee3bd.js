f44f336598e8077f302c89c3ea6f6b444c564a36
Adriel Espaillat <adrielespa@adriels-mbp-2.lan>
2020-08-08 09:29:25 -0400

    Updates

---
+++ precache-manifest.3fe4c741b9d5714a6d3a4520855ee3bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a47bc62145fa2f678da261aa2a3dadb8",
+    "url": "/CovidTracker2020/index.html"
+  },
+  {
+    "revision": "19baba5064817d95b1ca",
+    "url": "/CovidTracker2020/static/css/main.d48166ea.chunk.css"
+  },
+  {
+    "revision": "ef1befcb7c28f715e576",
+    "url": "/CovidTracker2020/static/js/2.8d9da2dc.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/CovidTracker2020/static/js/2.8d9da2dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19baba5064817d95b1ca",
+    "url": "/CovidTracker2020/static/js/main.ed0df2c2.chunk.js"
+  },
+  {
+    "revision": "9cae001b4904520fc0fb",
+    "url": "/CovidTracker2020/static/js/runtime-main.b04174fd.js"
+  }
+]);
